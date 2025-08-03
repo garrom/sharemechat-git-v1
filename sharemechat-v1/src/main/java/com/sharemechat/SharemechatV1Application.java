c3ed67f0a36dd39927a6d8a2a0ef9b8740ceedc2
Alain <alain_garmendia2002@yahoo.com>
2025-08-04 01:36:26 +0200

    Primer commit: agregar frontend y backend sin node_modules

---
+++ sharemechat-v1/src/main/java/com/sharemechat/SharemechatV1Application.java
@@ -0,0 +1,13 @@
+package com.sharemechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharemechatV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharemechatV1Application.class, args);
+	}
+
+}
---
+++ sharemechat-v1/src/test/java/com/sharemechat/SharemechatV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharemechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharemechatV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
