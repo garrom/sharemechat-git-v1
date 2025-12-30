f1bb88d99ecb543dacb902e2d640d6dbbb673c15
Alain <alain_garmendia2002@yahoo.com>
2025-12-30 09:46:21 +0100

    Ok Migracion a S3+CloudFront

--- sharemechat-v1/src/main/java/com/sharemechat/security/SecurityConfig.java
+++ sharemechat-v1/src/main/java/com/sharemechat/security/SecurityConfig.java
@@ -102,7 +102,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://test.sharemechat.com","http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://www.test.sharemechat.com","https://test.sharemechat.com","http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
